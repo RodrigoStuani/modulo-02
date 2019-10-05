4847a10423962eacacdd71e833f3d3e7244849ea
Rodrigo Stuani <stuani.developer@gmail.com>
2019-10-04 23:47:04 -0300

    Icluindo na pasta controller objeto de sessão.

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,32 @@
+import jwt from 'joswebtoken';
+
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Usuário não encontrado.' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({
+        error: "passaword não funciona";
+      })
+    }
+
+    const {id, name} = user;
+
+    return res.json({
+      user: {
+        id, name, email,
+      },
+      token: jwt.sign({id}, 'hauhsdjahsjldhalskjdhalksdh')
+    })
+  }
+}
+
+export default new SessionController();
