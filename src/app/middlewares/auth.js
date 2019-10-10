e09b47cc02ff5952d404d2c7d42f9168d6cc38f3
Rodrigo Stuani <stuani.developer@gmail.com>
2019-10-09 23:10:34 -0300

    Ajustado verificação da autenticação.

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,25 +1,26 @@
 import jwt from 'jsonwebtoken';
-import { promisify } from 'util';
+import { promisify } from 'util'; // Transforma uma função de callback em uma função async ou await
 
 import authConfig from '../../config/auth';
 
 export default async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization; // Buscando a autenticação.
 
   if (!authHeader) {
-    return res.status(401).json({ error: 'Token não foi emulado.' });
+    return res.status(401).json({ error: 'Token não foi enviado.' });
   }
 
-  const [, token] = authHeader.split(' ');
+  const [, token] = authHeader.split(' '); // Desestruturando para utilizar apenas o token após a virgula.
 
   try {
+    // Utilizado duas funções no método promisify(dentro qual o método que irá verificar)(Os parametros são chamados para verificar).
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    console.log(decoded);
+    req.UserId = decoded.id;
 
-    req.UserId();
+    // console.log(decoded);
     return next();
-  } catch (error) {
+  } catch (err) {
     return res.status(401).json({ error: 'Toque não válido' });
   }
 };
