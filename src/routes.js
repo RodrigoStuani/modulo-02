6f8962db727b3527d146e0933cb65a6331962bac
Rodrigo Stuani <stuani.developer@gmail.com>
2019-10-09 23:18:12 -0300

    Removido rota get.

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-// import User from './app/models/User'; rota para teste
-
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -10,20 +8,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.use(authMiddleware); // Só executa as rotas após o middlwware AuthMiddleware.
 
 routes.put('/users', authMiddleware, UserController.update);
-
-/*
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rodrigo Stuani',
-    email: 'rodrigo_stuani@outlook.com',
-    password_hash: '4687',
-  });
-
-  return res.json(user);
-});
-*/
 
 export default routes;
