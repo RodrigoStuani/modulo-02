069735927e0fae436ec66898ebc80a1f5061c1d8
Rodrigo Stuani <stuani.developer@gmail.com>
2019-10-12 22:59:24 -0300

    Implementado update do password.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,9 +19,29 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const { email, oldPassaword } = req.body;
 
-    return res.json({ ok: true });
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'Email já cadastrado!' });
+      }
+    }
+
+    if (oldPassaword && !(await user.checkPassword(oldPassaword))) {
+      return res.status(401).json({ error: 'Password não bate!' });
+    }
+
+    const { id, name, provider } = await User.update(req.body);
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
